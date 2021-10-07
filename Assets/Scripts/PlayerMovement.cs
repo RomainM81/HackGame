f6f47c5728315d063922a1ff1e3d53b13819be8e
RomainM81 <dcromain112@gmail.com>
2021-10-07 16:24:39 +0100

    add script for jump and run on Freya

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    void Update()
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
     {
+
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump") && isGrounded == true){
+            isJumping = true;
+        }
+
+        if (isGrounded == true) {
+            animator.SetBool("AnimJump", false);
+        }
+
+        if (isGrounded == false) {
+            animator.SetBool("AnimJump", true);
+        }
+
+
+        MovePlayer(horizontalMovement);
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speedo", characterVelocity);
+    }
+
+    void MovePlayer(float _horizontalMovement) {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
         
+        if(isJumping == true) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        } 
+    }
+
+    void Flip(float _velocity){
+        if(_velocity > 0.1f) {
+            spriteRenderer.flipX = false;
+        } else if(_velocity < -0.1f) {
+            spriteRenderer.flipX = true;
+        }
     }
 }
